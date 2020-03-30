4fda7aac54044f4fb261ddecf73a659ec578eec5
Matt <matthave@gmail.com>
2020-03-30 04:17:03 +0200

    Updates

---
+++ precache-manifest.5835716ff8088881372ba73bd7c95fa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9be11aa20a472e9df467ddd17ba714a",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "3d17932e96248f380627",
+    "url": "/covid19/static/css/main.34c4c046.chunk.css"
+  },
+  {
+    "revision": "91d7d2a0fa7ee71bc6fe",
+    "url": "/covid19/static/js/2.ef2e8d88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.ef2e8d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d17932e96248f380627",
+    "url": "/covid19/static/js/main.477cda1a.chunk.js"
+  },
+  {
+    "revision": "6c2eaaa7fcf880f8489d",
+    "url": "/covid19/static/js/runtime-main.e34f569d.js"
+  },
+  {
+    "revision": "d7d067771b01cdd88d0bc7125359e787",
+    "url": "/covid19/static/media/NewLogoSmallTransparent.d7d06777.png"
+  }
+]);
